9a791b538a96c0fc6071679175e94504be90364c
Júlia Strauch <jmstrauch04@gmail.com>
2024-06-27 12:11:08 -0300

    personalization

--- script.js
+++ script.js
@@ -9,19 +9,4 @@ function toggleMode() {
   // }
 
   html.classList.toggle('light');
-
-  // (2) mudar src de acordo com o theme
-  const avatar = document.querySelector('#profile img');
-  if (html.classList.contains('light')) {
-    avatar.setAttribute('src', './assets/avatar-light.png');
-  } else {
-    avatar.setAttribute('src', './assets/avatar.png');
-  }
-
-  // (3) mudar o alt de acordo com o theme
-  if (html.classList.contains('light')) {
-    avatar.setAttribute('alt', 'Light Mode');
-  } else {
-    avatar.setAttribute('alt', 'Dark Mode');
-  }
 }
